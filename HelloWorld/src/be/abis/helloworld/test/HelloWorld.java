8317e734e8ae8a36709d54cac44baa2af59f1094
zeghersbruno <bruno.zeghers@gmail.com>
2020-09-28 10:06:25 +0200

    my first commit for this project

---
+++ HelloWorld/src/be/abis/helloworld/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package be.abis.helloworld.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello " + args[0]);
+	}
+}
